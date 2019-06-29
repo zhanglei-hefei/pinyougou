35112bb61ab59e2662bc3dc2c4e5b1b99d693658
zhanglei <1310101914@qq.com>
2019-06-29 14:05:14 +0800

    品优购初始版本

---
+++ pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/shop/controller/BrandController.java
@@ -0,0 +1,24 @@
+package com.pinyougou.shop.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.service.BrandService;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+	
+	@Reference
+	private BrandService brandService;
+	
+	@RequestMapping("/findAll.do")
+	public List<TbBrand> findAll(){
+		return brandService.findAll();
+	}
+
+}
